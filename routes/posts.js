9a27bb565fe25a643cb2449c367172bc59d0afde
陈明槐 <chenminghuai@chenminghuaideMacBook-Pro.local>
2018-07-21 17:09:12 +0800

    模板修改

--- routes/posts.js
+++ routes/posts.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET posts page. */
 router.get('/', function(req, res, next) {
-  res.render('posts', { title: 'posts' });
+  res.render('posts', { title: 'posts', postsList: ['文章1', '文章2', '文章3'] });
 });
 
 module.exports = router;
