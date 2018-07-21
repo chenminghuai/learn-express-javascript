40a65b8cd9566bcc37644de21b826e87b7a6417d
陈明槐 <chenminghuai@chenminghuaideMacBook-Pro.local>
2018-07-21 17:45:57 +0800

    路由归类

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var postsRouter = require('./routes/posts');
+var pageRouter = require('./route.page');
+var apiRouter = require('./route.api');
 
 var app = express();
 
@@ -20,9 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/posts', postsRouter);
+app.use('/', pageRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- route.page.js
+++ route.page.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET posts page. */
+router.get('/posts', function(req, res, next) {
+  res.render('posts', { title: 'posts'} );
+});
+
 module.exports = router;
