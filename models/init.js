bb71284d0e3345b64be87ac6196e2ff3372e2744
陈明槐 <chenminghuai@chenminghuaideMacBook-Pro.local>
2018-07-24 11:11:39 +0800

    权限控制

--- models/init.js
+++ models/init.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
+var config = require('../config');
 
-mongoose.connect('mongodb://localhost:27017/firstapp', {
-    useNewUrlParser: true 
+mongoose.connect(config.mongodbUrl, {
+    useNewUrlParser: true
 });
