1a671df361bad448c55ad83d3ee58b7303951f9c
陈明槐 <chenminghuai@chenminghuaideMacBook-Pro.local>
2018-07-24 11:49:56 +0800

    更好的错误处理中心

--- app.js
+++ app.js
@@ -41,7 +41,19 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.format({
+    json() {
+      res.send({ error: err.toString() } );
+    },
+    html() {
+      res.render('error');
+    },
+
+    default() {
+      const message = `${errorDetails}`;
+      res.send(`500 Internal Server error:\n${err.toString}`);
+    },
+  })
 });
 
 module.exports = app;
